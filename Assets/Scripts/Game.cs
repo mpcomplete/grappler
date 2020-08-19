109a8168b368aea7d211ca95c6b715f17870b0cd
Steve Kane <kanesteven@gmail.com>
2020-08-19 11:52:17 -0500

    slope tests includes slope motion

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,30 +4,36 @@ public class Game : MonoBehaviour {
   Player player;
   SmartCamera smartCamera;
 
-
   void Start() {
     player = FindObjectOfType<Player>();
     smartCamera = FindObjectOfType<SmartCamera>();
   }
 
   void Update() {
-    if (player.CharacterController.isGrounded) {
-      smartCamera.LookAt(player.transform);
-    }
   }
 
   void FixedUpdate() {
-    float dt = Time.deltaTime;
-    Vector3 motion = Physics.gravity;
+    float dt = Time.fixedDeltaTime;
+    Vector3 direction = Physics.gravity.normalized;
+    Vector3 position = player.transform.position;
+    Ray downRay = new Ray(position, direction);
 
-    player.Velocity.x = 0;
-    player.Velocity.z = 0;
-    if (Input.GetKey(KeyCode.A)) {
-      motion += -player.Speed * Vector3.forward;
-    } else if (Input.GetKey(KeyCode.D)) {
-      motion += player.Speed * Vector3.forward;
+    if (player.CharacterController.isGrounded && Physics.Raycast(downRay, out RaycastHit hit, 10f)) {
+      Vector3 tangent = new Vector3(0, -hit.normal.z, hit.normal.y);
+
+      Debug.Log("Grounded");
+      Debug.DrawRay(position, direction, Color.blue);
+      Debug.DrawRay(position, hit.normal, Color.green);
+      Debug.DrawRay(position, tangent, Color.red);
+      player.transform.forward = direction;
+      player.transform.up = hit.normal;
+      player.Velocity = Vector3.Project(player.Velocity + dt * Physics.gravity, tangent);
+    } else {
+      Debug.Log("Air");
+      player.Velocity += Physics.gravity * dt;
     }
-    player.Velocity += motion * dt;
+
     player.CharacterController.Move(player.Velocity);
+    smartCamera.LookAt(player.transform);
   }
 }
